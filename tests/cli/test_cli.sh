e19a30506d8ac8908fdfd1dd9a216f13341780f6
Peter Marreck <lumbergh@gmail.com>
2026-02-20 12:38:24 -0500

    Add CLI tests for metadata trailer storage and restoration

--- tests/cli/test_cli.sh
+++ tests/cli/test_cli.sh
@@ -23,6 +23,28 @@ mode_src="$(stat --printf='%a' "$tmp_dir/input.txt")"
 mode_dst="$(stat --printf='%a' "$tmp_dir/out-a/input.txt")"
 [[ "$mode_src" == "$mode_dst" ]]
 
+list_a="$("./zig-out/bin/compact-pro" list "$tmp_dir/a.cpt")"
+[[ "$list_a" == *"input.txt"* ]]
+[[ "$list_a" != *".compact-pro.meta.bin"* ]]
+
+trailer_magic="$(xxd -p -l 8 -s -16 "$tmp_dir/a.cpt" | tr -d '\n')"
+[[ "$trailer_magic" == "43505854524c5231" ]]
+trailer_version="$(xxd -p -l 4 -s -4 "$tmp_dir/a.cpt" | tr -d '\n')"
+[[ "$trailer_version" == "01000000" ]]
+
+payload_len_le="$(xxd -p -l 4 -s -8 "$tmp_dir/a.cpt" | tr -d '\n')"
+payload_len_be="${payload_len_le:6:2}${payload_len_le:4:2}${payload_len_le:2:2}${payload_len_le:0:2}"
+payload_len=$((16#$payload_len_be))
+archive_size=$(wc -c < "$tmp_dir/a.cpt")
+base_size=$((archive_size - payload_len - 16))
+dd if="$tmp_dir/a.cpt" of="$tmp_dir/a-notrailer.cpt" bs=1 count="$base_size" status=none
+
+mkdir -p "$tmp_dir/out-a-notrailer"
+./zig-out/bin/compact-pro expand "$tmp_dir/a-notrailer.cpt" -d "$tmp_dir/out-a-notrailer"
+cmp "$tmp_dir/input.txt" "$tmp_dir/out-a-notrailer/input.txt"
+mode_no_trailer="$(stat --printf='%a' "$tmp_dir/out-a-notrailer/input.txt")"
+[[ "$mode_no_trailer" != "$mode_src" ]]
+
 printf 'second file\n' >"$tmp_dir/second.txt"
 ./zig-out/bin/compact-pro add "$tmp_dir/a.cpt" "$tmp_dir/second.txt"
 mkdir -p "$tmp_dir/out-b"
